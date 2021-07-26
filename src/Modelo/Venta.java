d253d19565e35583015acb1d8a4be89d808181a9
Angel Sifuentes <71534078+VidaInformatico@users.noreply.github.com>
2021-07-26 10:03:29 -0500

    Update Venta.java

--- src/Modelo/Venta.java
+++ src/Modelo/Venta.java
@@ -4,6 +4,7 @@ package Modelo;
 public class Venta {
     private int id;
     private int cliente;
+    private String nombre_cli;
     private String vendedor;
     private double total;
     private String fecha;
@@ -12,9 +13,10 @@ public class Venta {
         
     }
 
-    public Venta(int id, int cliente, String vendedor, double total, String fecha) {
+    public Venta(int id, int cliente, String nombre_cli, String vendedor, double total, String fecha) {
         this.id = id;
         this.cliente = cliente;
+        this.nombre_cli = nombre_cli;
         this.vendedor = vendedor;
         this.total = total;
         this.fecha = fecha;
@@ -34,6 +36,14 @@ public class Venta {
 
     public void setCliente(int cliente) {
         this.cliente = cliente;
+    }
+
+    public String getNombre_cli() {
+        return nombre_cli;
+    }
+
+    public void setNombre_cli(String nombre_cli) {
+        this.nombre_cli = nombre_cli;
     }
 
     public String getVendedor() {
@@ -59,6 +69,8 @@ public class Venta {
     public void setFecha(String fecha) {
         this.fecha = fecha;
     }
+
+    
 
     
 }
